9ec79f489bc3c71597a2dfaab224d55a979e045d
Facundo Voncina <facundo.voncina@bestday.com>
2019-08-10 11:22:24 -0500

    Clean up

--- Microsoft.AspNetCore.DataProtection.GoogleCloudStorage/GcsXmlRepository.cs
+++ Microsoft.AspNetCore.DataProtection.GoogleCloudStorage/GcsXmlRepository.cs
@@ -26,12 +26,6 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
         private readonly string _bucketName;
         private readonly string _name;
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="AzureBlobXmlRepository"/>.
-        /// </summary>
-        /// <param name="gcsClientFactory">A factory which can create <see cref="ICloudBlob"/>
-        /// instances. The factory must be thread-safe for invocation by multiple
-        /// concurrent threads, and each invocation must return a new object.</param>
         public GcsXmlRepository(Func<StorageClient> gcsClientFactory, string bucketName, string name = "keys")
         {
             _gcsClientFactory = gcsClientFactory ?? throw new ArgumentNullException(nameof(gcsClientFactory));
@@ -40,19 +34,13 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
             _random = new Random();
         }
 
-        /// <inheritdoc />
         public IReadOnlyCollection<XElement> GetAllElements()
         {
             var storageClient = CreateFreshStorageClient();
-
-            // Shunt the work onto a ThreadPool thread so that it's independent of any
-            // existing sync context or other potentially deadlock-causing items.
-
             var elements = Task.Run(() => GetAllElementsAsync(storageClient)).GetAwaiter().GetResult();
             return new ReadOnlyCollection<XElement>(elements);
         }
 
-        /// <inheritdoc />
         public void StoreElement(XElement element, string friendlyName)
         {
             if (element == null)
@@ -61,10 +49,6 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
             }
 
             var storageClient = CreateFreshStorageClient();
-
-            // Shunt the work onto a ThreadPool thread so that it's independent of any
-            // existing sync context or other potentially deadlock-causing items.
-
             Task.Run(() => StoreElementAsync(storageClient, element)).GetAwaiter().GetResult();
         }
 
@@ -87,9 +71,6 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
 
         private StorageClient CreateFreshStorageClient()
         {
-            // ICloudBlob instances aren't thread-safe, so we need to make sure we're working
-            // with a fresh instance that won't be mutated by another thread.
-
             var storageClient = _gcsClientFactory();
             if (storageClient == null)
             {
@@ -105,31 +86,15 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
 
             if (data == null)
             {
-                // no data in blob storage
                 return new XElement[0];
             }
-
-            // The document will look like this:
-            //
-            // <root>
-            //   <child />
-            //   <child />
-            //   ...
-            // </root>
-            //
-            // We want to return the first-level child elements to our caller.
-
             var doc = CreateDocumentFromBlob(data.BlobContents);
             return doc.Root.Elements().ToList();
         }
 
         private async Task<BlobData> GetLatestDataAsync(StorageClient storageClient)
         {
-            // Set the appropriate AccessCondition based on what we believe the latest
-            // file contents to be, then make the request.
-
             var latestCachedData = Volatile.Read(ref _cachedBlobData); // local ref so field isn't mutated under our feet
-
             try
             {
                 using (var memoryStream = new MemoryStream())
@@ -149,41 +114,30 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
                 latestCachedData = null;
                 Volatile.Write(ref _cachedBlobData, latestCachedData);
             }
-
             return latestCachedData;
         }
 
         private int GetRandomizedBackoffPeriod()
         {
-            // returns a TimeSpan in the range [0.8, 1.0) * ConflictBackoffPeriod
-            // not used for crypto purposes
             var multiplier = 0.8 + ( _random.NextDouble() * 0.2 );
             return (int)( multiplier * ConflictBackoffPeriod.Ticks );
         }
 
         private async Task StoreElementAsync(StorageClient storageClient, XElement element)
         {
-            // holds the last error in case we need to rethrow it
             ExceptionDispatchInfo lastError = null;
 
             for (var i = 0; i < ConflictMaxRetries; i++)
             {
                 if (i > 1)
                 {
-                    // If multiple conflicts occurred, wait a small period of time before retrying
-                    // the operation so that other writers can make forward progress.
                     await Task.Delay(GetRandomizedBackoffPeriod());
                 }
 
                 if (i > 0)
                 {
-                    // If at least one conflict occurred, make sure we have an up-to-date
-                    // view of the blob contents.
                     await GetLatestDataAsync(storageClient);
                 }
-
-                // Merge the new element into the document. If no document exists,
-                // create a new default document and inject this element into it.
 
                 var latestData = Volatile.Read(ref _cachedBlobData);
                 var doc = ( latestData != null )
@@ -191,7 +145,6 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
                     : new XDocument(new XElement(RepositoryElementName));
                 doc.Root.Add(element);
 
-                // Turn this document back into a byte[].
                 using (var serializedDoc = new MemoryStream())
                 {
                     doc.Save(serializedDoc, SaveOptions.DisableFormatting);
@@ -217,15 +170,16 @@ namespace Microsoft.AspNetCore.DataProtection.GoogleCloudStorage
                 }
             }
 
-            // if we got this far, something went awry
             lastError.Throw();
         }
 
         private sealed class BlobData
         {
+
             internal byte[] BlobContents { get; set; }
 
             internal string ETag { get; set; }
+
         }
     }
 }
